edb40dd76a58f022de63eb38f772a162015e691e
ByZoom <by.zoom69@gmail.com>
2022-02-01 00:28:00 +0100

    last modif de la soirée

--- ecommerce/src/components/products/Products.jsx
+++ ecommerce/src/components/products/Products.jsx
@@ -4,14 +4,7 @@ import { Grid } from "@material-ui/core";
 
 import useStyles from './styles';
 
-const products = [
-    { id: 1, name: "DESIRE1", price:"17", description:"tezgzgegegezgegerk", image:"https://static1.puretrend.com/articles/6/17/08/36/@/1867087-lupita-nyong-o-egerie-lancome-624x600-1.jpg"},
-    { id: 2, name: "DESIRE2", price:"67", description:"zgegezgegezgezgezg", image:"https://static1.puretrend.com/articles/6/17/08/36/@/1867087-lupita-nyong-o-egerie-lancome-624x600-1.jpg"},
-    { id: 3, name: "DESIRE3", price:"600", description:"egzegzgzeg", image:"https://static1.puretrend.com/articles/6/17/08/36/@/1867087-lupita-nyong-o-egerie-lancome-624x600-1.jpg"},
-    { id: 4, name: "DESIRE4", price:"12", description:"ezvzzegrggzegzeg", image:"https://static1.puretrend.com/articles/6/17/08/36/@/1867087-lupita-nyong-o-egerie-lancome-624x600-1.jpg"},
-];
-
-const Products = () => {
+const Products = ({ products, onAddToCart }) => {
     const classes = useStyles();
     return (
     <main className={classes.content}>
@@ -19,7 +12,7 @@ const Products = () => {
         <Grid container justifyContent="center" spacing={4}>
             {products.map((product) => (
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product product={product} />
+                    <Product product={product} onAddToCart={onAddToCart} />
                 </Grid>
             ))}
         </Grid>
--- ecommerce/src/components/products/product/Product.jsx
+++ ecommerce/src/components/products/product/Product.jsx
@@ -3,23 +3,24 @@ import {Card, CardMedia, CardContent, CardActions, Typography, IconButton} from 
 import { AddShoppingCart } from '@material-ui/icons';
 import useStyles from './styles'
 
-const Product = ({ product }) => {
+const Product = ({ product, onAddToCart }) => {
     const classes = useStyles();
+
     return(
         <Card className={classes.root}>
-            <CardMedia  className={classes.media} image={product.image} title={product.name} />
+            <CardMedia  className={classes.media} image={product.image.url} title={product.name} />
             <CardContent>
                 <div className={classes.CardContent}>
                     <Typography variant="h5" gutterBottom>
                         {product.name}
                     </Typography>
                     <Typography variant="h5" gutterBottom>
-                        {product.price}
+                        {product.price.formatted_with_symbol}
                     </Typography>
                 </div>
-                <Typography variant="body2" color="textSecondary">{product.description}</Typography>
+                <Typography dangerouslySetInnerHTML={{ __html: product.description}} variant="body2" color="textSecondary"></Typography>
                 <CardActions disableSpacing className={classes.CardActions}>
-                    <IconButton aria-label="Add to Card">
+                    <IconButton aria-label="Add to Card" onClick={() => onAddToCart('prod_mOVKl4GYgL5prR', 1)}>
                         <AddShoppingCart />
                     </IconButton>
                 </CardActions>
